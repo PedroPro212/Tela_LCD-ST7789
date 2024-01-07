3ce16b525529c988282a53973c01b4ac58c1b546
Pedro Oliveira <96023606+PedroPro212@users.noreply.github.com>
2024-01-07 17:57:30 -0300

    docs
    Documentação

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Tela_LCD-ST7789
+Olá pessoal! <br>
+Esse é um projeto voltado para o uso da tela LCD ST7789 no Arduino. <br>
+Resumindo, o projeto consiste em escrever um texto no Python que será enviado para o Arduino, que consequentemente irá aparecer na tela.
+Para o projeto vocês irão precisar de um Arduino, a tela LCD ST7789 240x240, e jumpers de conexão. 
 ## Portas conectadas
 GND -> GND <br>
 VCC -> 5V <br>
--- python/index.py
+++ python/index.py
@@ -1,4 +1,4 @@
-import serial
+import serial # Impotar a serial para comunicação
 import time
 
 # Abre a comunicação serial com o Arduino (substitua a porta e a velocidade conforme necessário)
@@ -9,11 +9,10 @@ time.sleep(2)  # Aguarda a inicialização da porta serial
 texto = input('Texto: ')  # Esperando o texto
 texto += '\n' # Adiciona \n para indicar o final da mensagem
 
-time.sleep(1)  # Pequeno atraso antes de enviar os dados
+time.sleep(0.1)  # Pequeno atraso antes de enviar os dados
 
 # Envia o texto para o Arduino
-arduino.write(texto.encode().upper())  # Envia o texto com a codificação necessária
-#print(arduino.write(texto.encode()))
+arduino.write(texto.encode('utf-8').upper())  # Envia o texto com a codificação necessária
 
 # Fecha a conexão serial
 arduino.close()
